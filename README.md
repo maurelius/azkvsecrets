a3495684d918b3ce34b03b39c0ff9ef497305388
Marc Cabanatuan <marc.cabanatuan@milwaukeetool.com>
2023-07-12 13:35:32 -0500

    Use the right script this time and update usage

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Script for retrieving secrets from Azure Key Vault
 I typically use this to retrieve API access keys from a specific KV. Add the script to your $PATH or keep it in the local working directory.
 
 ```python
-import azkvsecrets
+import az_kv_secrets
 from tenable.io import TenableIO
 # Define keys to auth to API
 accessKey, secretKey = azkvsecrets.get_keys()
--- az_kv_secrets.py
+++ az_kv_secrets.py
@@ -3,30 +3,29 @@
 """ Reference Documentation: https://learn.microsoft.com/en-us/azure/key-vault/secrets/quick-create-python?tabs=azure-cli """
 """ Azure Identity / DefaultAzureCredential Documentation: https://learn.microsoft.com/en-us/azure/developer/python/sdk/authentication-overview"""
 """ Currently must use Azure CLI for credentials to pass correctly """
-""" Don't forget to set az contect using AZ_SUBSCRIPTION variable """
+""" Don't forget to set az subscription context using AZ_SUBSCRIPTION variable """
+
+__name__ = "az_kv_secrets.py"
 
 import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 os.environ['KEY_VAULT_NAME'] = 'name-of-azure-keyvault'
-os.environ['AZ_RG_NAME'] = 'name-of-azure-resource-group'
-
-keyVaultName = os.environ["KEY_VAULT_NAME"]
-KVUri = f"https://{keyVaultName}.vault.azure.net"
-
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
-
 accessKeyName = 'name-of-secret'
 secretKeyName = 'name-of-secret'
+credential = DefaultAzureCredential()
+keyVaultName = os.environ["KEY_VAULT_NAME"]
+KVUri = f"https://{keyVaultName}.vault.azure.net"
+client = SecretClient(vault_url=KVUri, credential=credential)
 
-print(f"Retrieving your secrets from {keyVaultName}.")
+def get_accessKey():
+    return client.get_secret(accessKeyName).value
+        
+def get_secretKey():
+    return client.get_secret(secretKeyName).value
 
-accessKey = client.get_secret(accessKeyName)
-secretKey = client.get_secret(secretKeyName)
-
-print(f"Your access key is '{accessKey.value}'.")
-print(f"Your access key is '{secretKey.value}'.")
-
-print(" done.")
+def get_keys(): 
+    accessKey = get_accessKey() 
+    secretKey = get_secretKey() 
+    return accessKey, secretKey
