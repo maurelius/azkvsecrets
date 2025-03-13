2609d31ac9debef0e7a1fc724dfd505cddaeafd9
maurelius <homewrecker@gmail.com>
2025-03-13 13:32:44 -0500

    fix import error

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import os
+from az_kv_secrets import get_keys
 
 # Assuming your module is in the 'Azure' directory
 module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Azure'))
 sys.path.append(module_path)
 
-from Azure import get_keys  # Correct import
+# from Azure import get_keys  # Correct import
 
 accessKey, secretKey, linkingKey = get_keys() # Correct function call
 
